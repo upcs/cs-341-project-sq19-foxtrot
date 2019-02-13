3c89a4282ac320a6ea9aef1b99fb1370806b51a2
Polina Gannotskaya <42845358+gannotsk21@users.noreply.github.com>
2019-02-12 23:58:03 -0800

    Update login_functions.js

--- login_functions.js
+++ login_functions.js
@@ -1,3 +1,16 @@
 document.getElementById("submitting").onclick = function findUser() {
     //find user and find his info
 }
+
+function getUser(firstName) {
+	if (firstName !== null) {
+		console.log(`getting User from first Name: ${firstName}`);
+		return firstName;
+	} else {
+		return null;
+	}
+}
+
+module.exports = {
+	getUser
+};
