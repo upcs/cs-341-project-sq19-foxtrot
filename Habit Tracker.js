2f6459f102202d8afa58724735551c53a62b764f
Polina Gannotskaya <42845358+gannotsk21@users.noreply.github.com>
2019-02-13 00:24:02 -0800

    Update Habit Tracker.js

--- Habit Tracker.js
+++ Habit Tracker.js
@@ -1,20 +1,12 @@
 //test
 function btnClick() {
-    var a = 1;
-    var b = 0;
     var x = document.getElementById("mytable").getElementsByTagName("td");
     x[0].innerHTML = "Completed";
     x[0].style.backgroundColor = "yellow";  
-    return a+b;
 }
-
-module.exports={ btnClick };
 		
 function myFunction(x) {
     alert("Row index is: " + x.rowIndex);
     x[0].innerHTML = "Completed";
     x[0].style.backgroundColor = "yellow";    
-    return 1; 
-  }
-	 
-module.exports= { myFunction };
+}
