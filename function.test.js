56a9445d7c357389e8e178e128c36f43da9e62b1
Polina Gannotskaya <42845358+gannotsk21@users.noreply.github.com>
2019-02-13 07:56:46 -0800

    Update function.test.js

--- function.test.js
+++ function.test.js
@@ -1,16 +1,16 @@
 let login = require('./login_functions.js');
 
-//testing btnClick function from login_functions
+//testing getUser function from login_functions
 test('getting name', () => {
 	expect(login.getUser('Polina')).toBe('Polina');
 });
 
-//testing btnClick function from login_functions
+//testing getUser function from login_functions
 test('getting name', () => {
 	expect(login.getUser('Sarah')).toBe('Sarah');
 });
 
-//testing btnClick function from login_functions
+//testing getUser function from login_functions
 test('getting name', () => {
 	expect(login.getUser(null)).toBe(null);
 });
