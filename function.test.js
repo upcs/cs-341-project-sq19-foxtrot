ddda017d37495e7fdbc38b1837724f146d5c7e63
Polina Gannotskaya <42845358+gannotsk21@users.noreply.github.com>
2019-02-12 23:48:56 -0800

    Update function.test.js

--- function.test.js
+++ function.test.js
@@ -5,9 +5,3 @@ let journalFunct = require('./journal_functions.js');
 test('if button on habit tracker is clicked', () => {
     expect(habitTracker.btnClick().toBe(1));
 })
-
-//testing validateForm from journal_functions.js
-//checking if function correctly identifies an empty journal
-test('Check if forms say true when empty', () => {
-    expect(journalFunct.validateForm().toBe(false));
-})
