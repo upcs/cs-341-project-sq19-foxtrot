d592b751d65d08840477b022a2906b6db27ae366
Polina Gannotskaya <42845358+gannotsk21@users.noreply.github.com>
2019-02-12 23:29:19 -0800

    Update function.test.js

--- function.test.js
+++ function.test.js
@@ -1,5 +1,5 @@
-let habitTracker = require('Habit Tracker.js');
-let journalFunct = require('journal_functions.js');
+let habitTracker = require('./Habit Tracker.js');
+let journalFunct = require('./journal_functions.js');
 
 //testing btnClick function from Habit Tracker.js
 test('if button on habit tracker is clicked', () => {
