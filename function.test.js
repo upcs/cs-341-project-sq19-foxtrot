acf48937976bd6da4a4a929bfaf9ce0a75d9ae21
Polina Gannotskaya <42845358+gannotsk21@users.noreply.github.com>
2019-02-12 23:42:33 -0800

    Update function.test.js

--- function.test.js
+++ function.test.js
@@ -8,14 +8,6 @@ test('if button on habit tracker is clicked', () => {
 
 //testing validateForm from journal_functions.js
 //checking if function correctly identifies an empty journal
-document.forms["journalForm"]["fjournal"].value = "    ";
 test('Check if forms say true when empty', () => {
     expect(journalFunct.validateForm().toBe(false));
-})
-
-//testing validateForm from journal_functions.js
-//checking if function correctly identifies a non-empty journal
-document.forms["journalForm"]["fjournal"].value == "The journal is not empty";
-test('Check if forms say true when empty', () => {
-    expect(journalFunct.validateForm().toBe(true));
 })
