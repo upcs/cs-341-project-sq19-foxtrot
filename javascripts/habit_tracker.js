d78d6bba707b296c9fdbc67376393d7a3df3baeb
Joanna Hoang <hoangj21@up.edu>
2019-02-21 20:00:48 -0800

    Added functionaility to habit tracker
    Can now add rows to habit tracker and mark cells as complete

--- javascripts/habit_tracker.js
+++ javascripts/habit_tracker.js
@@ -1,12 +1,22 @@
 //test
-function btnClick() {
-    var x = document.getElementById("mytable").getElementsByTagName("td");
-    x[0].innerHTML = "Completed";
-    x[0].style.backgroundColor = "yellow";  
+function mark_cell(x) {
+    x.innerHTML = "Completed";
+    x.style.backgroundColor = "#bf7fff";    
 }
-		
-function myFunction(x) {
-    alert("Row index is: " + x.rowIndex);
-    x[0].innerHTML = "Completed";
-    x[0].style.backgroundColor = "yellow";    
+
+function add_row() {
+
+    var x=document.getElementById('myTable');
+    var new_row = x.rows[1].cloneNode(true);                      
+    new_row.cells[0].innerHTML = "Habit";
+    var num_columns = 8
+    for (i = 1; i <num_columns; i++) {    
+        new_row.cells[i].style.backgroundColor =  "#d9b3ff";
+        new_row.cells[i].innerHTML =  ""; 
+     }
+    new_row.cells[1].style.backgroundColor =  "#d9b3ff";
+    new_row.cells[1].innerHTML =  "";
+    x.appendChild( new_row );
+    var table = document.getElementById("myTable");
+    
 }
