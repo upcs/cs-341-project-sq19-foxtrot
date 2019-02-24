532d8e2f7b66840bd83243a5d56c93305d5e7d13
Sarah Schibel <schibel21@up.edu>
2019-02-23 17:13:04 -0800

    Created Extended Journal page and created functionality to stop user from inputting too many characters

--- javascripts/first_journal_functions.js
+++ javascripts/first_journal_functions.js
@@ -5,6 +5,11 @@ function validateForm() {
       return false;
     }
 
+    else if(x.length > 100){
+    alert("This is by far too many characters! The amount of characters necessary for this journal is 100. If you like to write more, click on the extended journal button below!");
+    return false; 
+    }
+
 
     alert("You've submitted your journal!");
     return (true);
