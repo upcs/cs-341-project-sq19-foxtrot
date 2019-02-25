17b0ead4b4d500778a4a21605d9334d267ad213b
Sarah Schibel <schibel21@up.edu>
2019-02-25 10:00:12 -0800

    checking again for things

---
+++ javascripts/extended_journal_functions.js
@@ -0,0 +1,13 @@
+function validateForm() {
+    var x = document.forms["journalForm"]["fjournal"].value;
+    if (x.length == 0) {
+      alert("You need to put something in your journal!");
+      return false;
+    }
+
+
+    alert("You've submitted your journal!");
+    return (true);
+   
+  }
+
