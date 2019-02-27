d748596bf7686dd9b6dd4381876d4b12b430168d
Polina Gannotskaya <gannotsk21@up.edu>
2019-02-26 18:54:19 -0800

    Update function.test.js

--- function.test.js
+++ function.test.js
@@ -1,16 +1,39 @@
-let login = require('./login_functions.js');
+let login = require('./javascripts/login_functions.js');
 
-//testing getUser function from login_functions
-test('getting name', () => {
-	expect(login.getUser('Polina')).toBe('Polina');
+//testing login function from login_functions
+
+document.myform.psw.value = "foxtrot";
+document.myform.uname.value = "";
+test("given empty username", () => {
+	expect(validateLogin()).toBe(false);
 });
 
-//testing getUser function from login_functions
-test('getting name', () => {
-	expect(login.getUser('Sarah')).toBe('Sarah');
+document.myform.psw.value = "";
+document.myform.uname.value = "foxtrot";
+test("given empty password", () => {
+	expect(validateLogin()).toBe(false);
 });
 
-//testing getUser function from login_functions
-test('getting name', () => {
-	expect(login.getUser(null)).toBe(null);
+document.myform.psw.value = "foxtrot";
+document.myform.uname.value = "foxtrot";
+test("given wrong password", () => {
+	expect(validateLogin()).toBe(false);
+});
+
+document.myform.psw.value = "polina";
+document.myform.uname.value = "foxtrot";
+test("given wrong case username", () => {
+	expect(validateLogin()).toBe(false);
+});
+
+document.myform.psw.value = "Polina";
+document.myform.uname.value = "Foxtrot";
+test("given wrong case password", () => {
+	expect(validateLogin()).toBe(false);
+});
+
+document.myform.psw.value = "Polina";
+document.myform.uname.value = "foxtrot";
+test("given correct login info", () => {
+	expect(validateLogin()).toBe(true);
 });
