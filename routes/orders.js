5327d65461e8ac90a81ca74ac82a2cbb35514f25
ashika-m <mulagada21@up.edu>
2019-02-28 18:17:10 -0800

    Database is working!!
    still need to format, only working on the journal page

--- routes/orders.js
+++ routes/orders.js
@@ -1,29 +1,34 @@
+/*
+Author: Ashika Mulagada, Sarah Schibel, Polina, Joana
+*/
+
 var express = require('express');
 var router = express.Router();
 var dbms = require("./dbms.js");
 
 
 
- 
+router.post('/', function(req, res, next) {
+    console.log("In orders.js post");
+    dbms.dbquery("SELECT Journal_Mon FROM leadership",
+    function(err, data){
+        queryData(data, res);
+    }
+    );
+
+});
+
 function queryData(data, res){
-    console.log("made it to this place");
+    console.log("In orders.js query data");
     var array = [];
-    
+
     for(var i = 0; i<data.length; i++)
     {
 	    array.push({Journal_Mon: data[i].Journal_Mon});
     }
     res.json(array);
-  
+
 }
 
-router.post('/', function(req, res, next) {
-    
-    dbms.dbquery("SELECT Journal_Mon FROM leadership",
-		 function(err, data){
-		     queryData(data, res);
-		 }
-    );
-    
-});
+
 module.exports = router;
