08dcc9e353cb1fc6094f0fd1fb5b6a047c51793c
Sarah Schibel <schibel21@up.edu>
2019-03-25 07:08:37 +0900

    Added to the insert router

--- public/javascripts/first_journal_functions.js
+++ public/javascripts/first_journal_functions.js
@@ -1,3 +1,5 @@
+date = new Date;
+day = date.getDay();
 
 function validateForm() {
     var x = document.forms["journalForm"]["fjournal"].value;
@@ -9,10 +11,31 @@ else if (x.length > 100) {
 alert("This is by far too many characters! The amount of characters necessary for this journal is 100. If you like to write more, click on the extended journal button below!");
 return false;
 }
-
-$.post("/newInserts", {Journal_Mon:x})
-alert("You've submitted your journal!");
-return (true);
+else if (day = 1){
+    $.post("/newInserts", {Journal_Mon:x})
+    alert("You've submitted your journal!");
+    return (true);
+    }
+else if (day = 2){
+    $.post("/newInserts", {Journal_Tue:x})
+    alert("You've submitted your journal!");
+    return (true);
+    }
+else if (day = 3){
+    $.post("/newInserts", {Journal_Wed:x})
+    alert("You've submitted your journal!");
+    return (true);
+    }
+else if (day = 4){
+    $.post("/newInserts", {Journal_Thu:x})
+    alert("You've submitted your journal!");
+    return (true);
+        }
+else if (day = 5){
+    $.post("/newInserts", {Journal_Fri:x})
+    alert("You've submitted your journal!");
+    return (true);
+    }
 }
 
 //added for tests
