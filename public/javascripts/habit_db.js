d81c5f5a65a54c0d9c22a96ec39e850e2d4dd4a6
ashika-m <mulagada21@up.edu>
2019-03-24 17:29:54 -0700

    Habit tracker display
    displays habits by week now

--- public/javascripts/habit_db.js
+++ public/javascripts/habit_db.js
@@ -8,10 +8,25 @@ function displayData(){
 		null,
 	   	function(data){
 
+
+         var x=document.getElementById('prevTable');
+
+
+
+         var habitWk = data[0].HabitWk;
+
+         $("#prevTable").prepend('<tr class="week"><td colspan= 8 > Week '+ habitWk +' </td></tr>');
+
+
+
 	       var jourVar = " ";
 
 	       for (var i = 0; i<data.length; i++)
 	       {
+           if(data[i].HabitWk != habitWk){
+             habitWk = data[i].HabitWk;
+             jourVar += '<tr class="week"><td colspan= 8 > Week '+ habitWk +' </td></tr>';
+           }
 			       jourVar += "<tr>"+"<td>" + data[i].Habit + "</td>"
 						 +"<td>"+ data[i].Mon +"</td>"
 		       +"<td>"+  data[i].Tue +"</td>"
@@ -25,8 +40,13 @@ function displayData(){
 
 	       $("#prevTable").append(jourVar);
 
+         var weekRows = document.getElementsByClassName("week");
+         console.log("Week row:" + weekRows);
+         for(var i=0; i<weekRows.length; i++) {
+           markWeekRow(weekRows[i]);
+         }
 
-         var x=document.getElementById('prevTable');
+
          var new_row;
          var cell;
 
@@ -34,9 +54,9 @@ function displayData(){
            new_row=x.rows[i];
            for(var j=0; j< new_row.cells.length; j++){
              cell=new_row.cells[j];
-               console.log(cell);
+               //console.log(cell);
              if(cell.innerHTML == 1){
-               console.log(cell);
+               //console.log(cell);
                mark_prevCell(cell);
              }
              else if(cell.innerHTML == 0){
@@ -51,8 +71,14 @@ function displayData(){
 
 }
 
+function markWeekRow(weekRow){
+  console.log("in markWeekRow");
+  weekRow.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+  weekRow.style.border = "10px solid rgba(0, 0, 0, 0.0)";
+}
+
 function mark_prevCell(x) {
-    console.log("marking cell");
+    //console.log("marking cell");
     x.innerHTML = "Completed";
     x.style.backgroundColor = "#bf7fff";
 }
