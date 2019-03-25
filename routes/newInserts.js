3f87da15793595d6e09b9f3da84f29680caa74f2
Sarah Schibel <schibel21@up.edu>
2019-03-24 19:27:54 -0700

    Worked on making Journal title changeable and inserting data

--- routes/newInserts.js
+++ routes/newInserts.js
@@ -2,87 +2,55 @@ var express = require('express');
 var router = express.Router();
 var dbms = require("./dbms.js");
 
-/*Posts from Monday Journal Entries*/ 
-router.post('/', function(req, res, next){
+date = new Date(2019, 2, 24);
+day = date.getDay();
+ 
+  router.post('/', function(req, res, next){
 
-  var journal_mon = req.body.Journal_Mon; 
-  // var journal_tue = req.body.Journal_Tue;
-  // var journal_wed = req.body.Journal_Wed;
-  // var journal_thu = req.body.Journal_Thu;
-  // var journal_fri = req.body.Journal_Fri;
+    var journal_mon = req.body.Journal_Mon; 
+    var journal_tue = req.body.Journal_Tue;
+    var journal_wed = req.body.Journal_Wed;
+    var journal_thu = req.body.Journal_Thu;
+    var journal_fri = req.body.Journal_Fri;
+    var journal_sat = req.body.Journal_Sat; 
+    var journal_sun = req.body.Journal_Sun; 
+  
+  if(day = 0){
+    dbms.dbquery("INSERT INTO leadership (Journal_Sun) VALUES ('"+ journal_sun +"')",
+    function(err, data){}
+    );}
 
+  else if(day = 1){
+    dbms.dbquery("INSERT INTO leadership (Journal_Mon) VALUES ('"+ journal_mon +"')",
+    function(err, data){}
+    );}
 
-  dbms.dbquery("INSERT INTO leadership (Journal_Mon) VALUES ('"+ 
-  journal_mon +"')",
-  function(err, data){}
-  );
-});
+  // else if(day = 2){
+  //   dbms.dbquery("INSERT INTO leadership (Journal_Tue) VALUES ('"+ journal_tue +"')",
+  //   function(err, data){}
+  //   );}
 
-/*Posts from Tuesday Journal Entries*/ 
-router.post('/', function(req, res, next){
+  // else if(day = 3){
+  //   dbms.dbquery("INSERT INTO leadership (Journal_Wed) VALUES ('"+ journal_wed +"')",
+  //   function(err, data){}
+  //   );}
+  
+  // else if(day = 4){
+  //   dbms.dbquery("INSERT INTO leadership (Journal_Thu) VALUES ('"+ journal_thu +"')",
+  //   function(err, data){}
+  //   );}
 
-  // var journal_mon = req.body.Journal_Mon; 
-  var journal_tue = req.body.Journal_Tue;
-  // var journal_wed = req.body.Journal_Wed;
-  // var journal_thu = req.body.Journal_Thu;
-  // var journal_fri = req.body.Journal_Fri;
+  // else if(day = 5){
+  //   dbms.dbquery("INSERT INTO leadership (Journal_Fri) VALUES ('"+ journal_fri +"')",
+  //   function(err, data){}
+  //   );}
 
+  // else if(day = 6){
+  //   dbms.dbquery("INSERT INTO leadership (Journal_Sat) VALUES ('"+ journal_sat +"')",
+  //   function(err, data){}
+  //   );}
 
-  dbms.dbquery("INSERT INTO leadership (Journal_Tue) VALUES ('"+ 
-  journal_tue +"')",
-  function(err, data){}
-  );
-});
+  });
+  
 
-/*Posts from Wednesday Journal Entries*/ 
-router.post('/', function(req, res, next){
-
-  // var journal_mon = req.body.Journal_Mon; 
-  // var journal_tue = req.body.Journal_Tue;
-  var journal_wed = req.body.Journal_Wed;
-  // var journal_thu = req.body.Journal_Thu;
-  // var journal_fri = req.body.Journal_Fri;
-
-
-  dbms.dbquery("INSERT INTO leadership (Journal_Wed) VALUES ('"+ 
-  journal_wed +"')",
-  function(err, data){}
-  );
-});
-
-/*Posts from Thursday Journal Entries*/ 
-router.post('/', function(req, res, next){
-
-  // var journal_mon = req.body.Journal_Mon; 
-  // var journal_tue = req.body.Journal_Tue;
-  // var journal_wed = req.body.Journal_Wed;
-  var journal_thu = req.body.Journal_Thu;
-  // var journal_fri = req.body.Journal_Fri;
-
-
-  dbms.dbquery("INSERT INTO leadership (Journal_Thu) VALUES ('"+ 
-  journal_thu +"')",
-  function(err, data){}
-  );
-});
-
-/*Posts from Friday Journal Entries*/ 
-router.post('/', function(req, res, next){
-
-  // var journal_mon = req.body.Journal_Mon; 
-  // var journal_tue = req.body.Journal_Tue;
-  // var journal_wed = req.body.Journal_Wed;
-  // var journal_thu = req.body.Journal_Thu;
-  var journal_fri = req.body.Journal_Fri;
-
-
-  dbms.dbquery("INSERT INTO leadership (Journal_fri) VALUES ('"+ 
-  journal_fri +"')",
-  function(err, data){}
-  );
-});
-
-// dbms.dbquery("INSERT INTO leadership (Journal_Mon, Journal_Tue, Journal_Wed, Journal_Thu, Journal_Fri) VALUES ('"+ 
-//   journal_mon +"', '"+ journal_tue +"', '"+ journal_wed +"', '"+ journal_thu +"', '"+ journal_fri +"')",
-//   function(err, data){}
 module.exports = router;
