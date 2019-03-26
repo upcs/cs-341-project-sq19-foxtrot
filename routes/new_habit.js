e19675b03c4f9eb2ed18976fa95c2de8bc52715e
Joanna Hoang <hoangj21@up.edu>
2019-03-25 20:12:07 -0700

    modified habit tracker files

--- routes/new_habit.js
+++ routes/new_habit.js
@@ -8,7 +8,7 @@ router.post('/', function(req, res, next) {
     var habit_name = req.body.habit_name; 
     var tracker = "Habit"
     //Add getter to return current user for username 
-    dbms.dbquery("INSERT INTO  leadership(username,tracker,name) VALUES ('"+username+ "','"+tracker+ "','"+habit_name+"')",
+    dbms.dbquery("INSERT INTO  leadership(username,tracker,name) VALUES ('"+tracker+"','"+habit_name+"')",
 function(err, data){ }
 );     
 });
--- routes/new_mark_habit.js
+++ routes/new_mark_habit.js
@@ -3,19 +3,18 @@ var router = express.Router();
 var dbms = require("./dbms.js");
 
 
-
 router.post('/', function(req, res, next) {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); 
     var yyyy = today.getFullYear();
-    today = mm + '/' + dd + '/' + yyyy;
+    today = mm + '_' + dd + '_' + yyyy;
     var input = 1; 
     var username = "";
     var habit = req.body.habit_name
     //Add getter to return current user for username 
     
-    dbms.dbquery("UPDATE  leadership SET '" + today + "'='" + input + "' WHERE Habit = '"+habit+"', Username = '"+username+"'",
+    dbms.dbquery("UPDATE  leadership SET '" + today + "'='" + input + "' WHERE Habit = '"+habit+"'",
 function(err, data){ }
 );     
 });
