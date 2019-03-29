69bc7a1d24d4fdc40a399ec1e951752ef399ef04
ashika-m <mulagada21@up.edu>
2019-03-29 09:41:30 -0700

    Working on habit tracker db stuff

--- public/javascripts/habit_db.js
+++ public/javascripts/habit_db.js
@@ -24,9 +24,124 @@ function displayData(){
 
          var x=document.getElementById('prevTable');
 
+         var weekday = getDayOfWeek(data[1].date);
+         console.log(weekday);
+
+         if(weekday == 'Monday'){
+           console.log('yes monday');
+         }
+
+         var result = getWeekNumber(data[1].date);
+
+         console.log('Week no:' + result);
+
+         //get corresponding Sunday, TODO get actual year from Date
+         //var sunday = getSundayFromWeekNum(result, 2019);
+        // console.log('Sunday date:' + sunday);
 
 
-         var habitWk = data[0].HabitWk;
+         //get corrspoding saturday
+         //var saturday = getSaturdayFromWeekNum(result, 2019);
+
+
+
+         //for loop through all data, if week num is not equal, make new week row
+            //loop through that row until getday matches and mark as completed
+         //else if habit doesn't exist in the table add another empty row to the table
+            //mark corresponding the row
+
+
+          //UPDATE
+          //do the week head the same way
+          //make table of set number of rows according to how many habits the person has
+          //
+
+
+          var habitWk = 0;
+          //var habitWk = getWeekNumber(data[0].date);
+          var sunday = getSundayFromWeekNum(habitWk, 2019);
+          var saturday = getSaturdayFromWeekNum(habitWk, 2019);
+          $("#prevTable").prepend('<tr class="week"><td colspan= 8 > Week '+ sunday + '-' + saturday +' </td></tr>');
+
+
+           //var habitnum = data[0].habitnum;
+           var habitnum = 4;
+   	       var jourVar = " ";
+
+   	       for (var i = 0; i<data.length; i++){
+              if(getWeekNumber(data[i].date) != habitWk){
+                console.log("in if statement");
+                habitWk = getWeekNumber(data[i].date);
+                var sunday = getSundayFromWeekNum(habitWk, 2019);
+                var saturday = getSaturdayFromWeekNum(habitWk, 2019);
+                jourVar += '<tr class="week"><td colspan= 8 > Week '+ sunday + '-' + saturday +' </td></tr>';
+                jourVar += "<tr>"+"<th>" + "Habit" + "</th>" + "<td>"+ "Sunday" +"</td>" + "<td>"+ "Monday" +"</td>" +"<td>"+ "Tuesday" +"</td>" +"<td>"+ "Wednesday" +"</td>" +"<td>"+ "Thursday" +"</td>"
+                +"<td>"+ "Friday" +"</td>" +"<td>"+ "Saturday" +"</td> </tr>";
+
+                for(var a=0; a<habitnum; a++){
+                  jourVar += "<tr>"+"<th>" + a + "<button class='editbtn' OnClick = 'removeRow()''>remove</button> </th>";
+                  jourVar += "<td>"+ 0 +"</td>" + "<td>"+ 0 +"</td>" +"<td>"+ 0 +"</td>" +"<td>"+ 0 +"</td>" +"<td>"+ 0 +"</td>" +"<td>"+ 0 +"</td>" +"<td>"+ 0 +"</td> </tr>";
+                }
+                /*for(var k = 0; k<7; k++){
+                  console.log("in loop" + k);
+                  if(j = data[i].date.getDay){
+                    console.log("mark day");
+                    jourVar += "<td>"+ 1 +"</td>";
+                  }
+                  else{
+                    console.log("dont mark day");
+                    jourVar += "<td>"+ 0 +"</td>";
+                  }
+                  console.log("en loop" + k);
+                }*/
+              }
+              /*else if(data[i].habit_number > habitnum){
+                habitnum = data[i].habitnum;
+                jourVar += "<tr>"+"<th>" + data[i].habit + "<button class='editbtn' OnClick = 'removeRow()''>remove</button> </th>";
+                jourVar += "<td>"+ 0 +"</td>" + "<td>"+ 0 +"</td>" +"<td>"+ 0 +"</td>" +"<td>"+ 0 +"</td>" +"<td>"+ 0 +"</td>" +"<td>"+ 0 +"</td>" +"<td>"+ 0 +"</td>";
+                jourVar += "</tr>";
+              }*/
+   	       }
+
+           $("#prevTable").append(jourVar);
+
+
+           var weekRows = document.getElementsByClassName("week");
+           console.log("Week row:" + weekRows);
+           for(var i=0; i<weekRows.length; i++) {
+             markWeekRow(weekRows[i]);
+           }
+
+           var new_row;
+           var cell;
+           for(var i=1; i< x.rows.length; i++){
+             new_row=x.rows[i];
+             for(var j=0; j< new_row.cells.length; j++){
+               cell=new_row.cells[j];
+                 //console.log(cell);
+               if(cell.innerHTML == 1){
+                 //console.log(cell);
+                 mark_prevCell(cell);
+               }
+               else if(cell.innerHTML == 0){
+                 cell.innerHTML = " ";
+               }
+             }
+           }
+
+         //for loop through entire table, if the corresponding habit has that date then make as complete
+
+
+         //else{
+           //get corresponding sunday
+         //}
+         //make a table for that day, mark (seperate function)
+
+         //go through other data either mark in table or make new table
+
+
+
+         /*var habitWk = data[0].HabitWk;
 
          $("#prevTable").prepend('<tr class="week"><td colspan= 8 > Week '+ habitWk +' </td></tr>');
 
@@ -76,12 +191,60 @@ function displayData(){
                cell.innerHTML = " ";
              }
            }
-         }
+         }*/
 
          //make table visible
 			   document.getElementById('prevTable').style.display='';
 	   }, "json");
 
+}
+
+//External citation: https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
+/*function getWeekNumber(d) {
+    // Copy date so don't modify original
+    d = new Date();
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    // Set to nearest Thursday: current date + 4 - current day number
+    // Make Sunday's day number 7
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    // Get first day of year
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    // Calculate full weeks to nearest Thursday
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    // Return array of year and week number
+    return weekNo;
+}*/
+
+function getWeekNumber(d) {
+    d = new Date();
+    var onejan = new Date(d.getFullYear(),0,1);
+    var millisecsInDay = 86400000;
+    console.log(Math.ceil((((d - onejan) /millisecsInDay) + onejan.getDay()+1)/7));
+    return Math.ceil((((d - onejan) /millisecsInDay) + onejan.getDay()+1)/7);
+};
+
+function getSundayFromWeekNum(weekNum, year) {
+    var sunday = new Date(year, 0, (1 + (weekNum - 1) * 7));
+    while (sunday.getDay() !== 0) {
+        sunday.setDate(sunday.getDate() - 1);
+    }
+    console.log(sunday);
+    return sunday;
+}
+
+function getSaturdayFromWeekNum(weekNum, year) {
+    var saturday = new Date(year, 0, (7 + (weekNum - 1) * 7));
+    while (saturday.getDay() !== 6) {
+        saturday.setDate(saturday.getDate() - 1);
+    }
+    console.log(saturday);
+    return saturday;
+}
+
+//External Citation: https://stackoverflow.com/questions/17964170/get-the-weekday-from-a-date-object-or-date-string-using-javascript
+function getDayOfWeek(date) {
+  var dayOfWeek = new Date(date).getDay();
+  return isNaN(dayOfWeek) ? null : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
 }
 
 function markWeekRow(weekRow){
--- routes/orders.js
+++ routes/orders.js
@@ -14,7 +14,7 @@ router.post('/', function(req, res, next) {
     //var username = 'Sarah';
     //var username = getUser();
     //console.log("username is:" + username);
-    dbms.dbquery("SELECT * FROM leadership WHERE Username='Ashika' AND Habit IS NOT NULL AND Journal_Mon IS NOT NULL",
+    dbms.dbquery("SELECT * FROM habit WHERE Username='Ashika'",
     function(err, data){
         queryData(data, res);
     }
@@ -26,16 +26,13 @@ function queryData(data, res){
     console.log(data);
     var array = [];
 
+
     for(var i = 0; i<data.length; i++)
     {
-	    array.push({Journal_Mon: data[i].Journal_Mon, Journal_Tue: data[i].Journal_Tue, Journal_Wed: data[i].Journal_Wed
-      ,Journal_Thu: data[i].Journal_Thu, Journal_Fri: data[i].Journal_Fri, HabitWk: data[i].HabitWk,
-      Habit: data[i].Habit, Mon: data[i].Mon,Tue: data[i].Tue, Wed: data[i].Wed, Thu: data[i].Thu,
-      Fri: data[i].Fri, Sat: data[i].Sat, Sun: data[i].Sun});
+	    array.push({date: data[i].adate, habit: data[i].habit, habit_number: data[i].habit_number});
     }
     console.log(array);
     res.json(array);
 }
-
 
 module.exports = router;
