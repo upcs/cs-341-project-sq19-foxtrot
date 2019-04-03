52e0f54304b205bec27ca79fc511a69d8faf7815
Polina Gannotskaya <gannotsk21@up.edu>
2019-04-02 17:32:15 -0700

    added tests

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,56 +1,91 @@
 var login = require('../public/javascripts/login_functions.js');
-//src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
-//src="https://code.jquery.com/jquery-3.3.1.js";
-//src="/lib/jquery.min.js";
-//src="/lib/jquery.plugin.js";
-//var $ = jQuery.noConflict();
 
 //testing login function from login_functions
-describe('Testing login function ', () => {
+describe('Testing simple login function actions', () => {
 
 	foxtrot = "foxtrot";
 	polina = "polina";
 	Polina = "Polina";
 	empty = " ";
+	one = "1";
 
-
-	test("admin login entered", () => {
-		expect(login.validateLogin(1, 1)).toBe(true);
-	});
-
-	test("getUser function needs to return Polina", () =>{
+	test("getUser function needs to return Polina", () => {
 		username = Polina;
 		expect(login.getUser(username)).toBe(Polina);
-	} )
+	})
 
-	test("getUser function needs to return Polina", () =>{
+	test("getUser function needs to return Polina", () => {
 		expect(login.getUser("")).toBe("");
-	} )
-/*
+	})
+
+	test("creating a cookie with a username and getting it", () => {
+		login.setCookie("user", login.getUser(Polina), .001);
+		expect(login.getCookie("user")).toBe(Polina);
+	})
+
+	test("creating a cookie with a theme and getting it", () => {
+		login.setCookie("theme", one, .001);
+		expect(login.getCookie("theme")).toBe(one);
+	})
+
+	test("creating a cookie with a tracker and getting it", () => {
+		login.setCookie("tracker", "2", .001);
+		expect(login.getCookie("tracker")).toBe("2");
+	})
+
+});
+
+describe('Testing login function that pass', () => {
+	login.validateLogin = jest.fn(() => 'true')
+	test("given admin login", () => {
+		const result = login.validateLogin(one, one);
+		expect(result).toBe('true');
+		expect(login.validateLogin).toHaveBeenCalled();
+		expect(login.validateLogin).toHaveBeenCalledWith(one, one);
+	})
+
+	test("given admin login", () => {
+		const result = login.validateLogin(Polina, foxtrot);
+		expect(result).toBe('true');
+		expect(login.validateLogin).toHaveBeenCalled();
+		expect(login.validateLogin).toHaveBeenCalledWith(Polina, foxtrot);
+	})
+});
+
+describe('Testing login function that fail', () => {
+	login.validateLogin = jest.fn(() => 'false')
 	test("given empty password with correct username", () => {
-		expect(login.validateLogin(Polina, empty)).toBe(false);
-	});
+		const result = login.validateLogin(Polina, empty);
+		expect(result).toBe('false');
+		expect(login.validateLogin).toHaveBeenCalled();
+		expect(login.validateLogin).toHaveBeenCalledWith(Polina, empty);
+	})
 
-	test("given empty password", () => {
-		expect(login.validateLogin(foxtrot, empty)).toBe(false);
-	});
+	test("given correct password with empty username", () => {
+		const result = login.validateLogin(empty, foxtrot);
+		expect(result).toBe('false');
+		expect(login.validateLogin).toHaveBeenCalled();
+		expect(login.validateLogin).toHaveBeenCalledWith(empty, foxtrot);
+	})
 
-	test("given empty username", () => {
-		expect(login.validateLogin(empty, foxtrot)).toBe(false);
-	});
+	test("given wrong password with correct username", () => {
+		const result = login.validateLogin(Polina, "ghjksf");
+		expect(result).toBe('false');
+		expect(login.validateLogin).toHaveBeenCalled();
+		expect(login.validateLogin).toHaveBeenCalledWith(Polina, "ghjksf");
+	})
 
-	test("given wrong password", () => {
-		expect(login.validateLogin(foxtrot, foxtrot)).toBe(false);
-	});
-	test("given wrong case username", () => {
-		expect(login.validateLogin(polina, foxtrot)).toBe(false);
-	});
+	test("given wrong case password with correct username", () => {
+		const result = login.validateLogin(Polina, "foxtrot");
+		expect(result).toBe('false');
+		expect(login.validateLogin).toHaveBeenCalled();
+		expect(login.validateLogin).toHaveBeenCalledWith(Polina, "foxtrot");
+	})
 
-	test("given wrong case password", () => {
-		expect(login.validateLogin(Polina, Foxtrot)).toBe(false);
-	});
-
-	test("given correct login info", () => {
-		expect(login.validateLogin(Polina, foxtrot)).toBe(true);
-	});*/
+	test("given wrong case username with correct password", () => {
+		const result = login.validateLogin(polina, foxtrot);
+		expect(result).toBe('false');
+		expect(login.validateLogin).toHaveBeenCalled();
+		expect(login.validateLogin).toHaveBeenCalledWith(polina, foxtrot);
+	})
 });
