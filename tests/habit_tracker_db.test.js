be75850dec4377d58e48ba34cfc746e4def6f39b
ashika-m <mulagada21@up.edu>
2019-04-04 17:02:22 -0700

    Update habit_tracker_db.test.js

--- tests/habit_tracker_db.test.js
+++ tests/habit_tracker_db.test.js
@@ -6,15 +6,15 @@ let habit = require('../public/javascripts/habit_db.js');
 
 
 test('add day function works with adding one',()=>{
-  expect( habit.addDays('2019-03-25T07:00:00.000Z', 1).toBe(new Date('2019-03-26T07:00:00.000Z')));
+  expect(addDays('2019-03-25T07:00:00.000Z', 1).toBe(new Date('2019-03-26T07:00:00.000Z')));
 });
 
 test('add day function works with end of month',()=>{
-  expect( habit.addDays('2019-03-30T07:00:00.000Z', 3).toBe(new Date('2019-04-02T07:00:00.000Z')));
+  expect(addDays('2019-03-30T07:00:00.000Z', 3).toBe(new Date('2019-04-02T07:00:00.000Z')));
 });
 
 test('Get week number works with valid date',()=>{
-  expect( habit.getWeekNumber('2019-03-25T07:00:00.000Z').toBe(13));
+  expect(getWeekNumber('2019-03-25T07:00:00.000Z').toBe(13));
 });
 
 test('Get week number works with valid date early in the year',()=>{
