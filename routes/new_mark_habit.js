0f3a81bd0d2e01670a6bbdb008996adf6bf458dc
ashika-m <mulagada21@up.edu>
2019-04-08 08:02:46 -0700

    MInor fixes

--- routes/new_mark_habit.js
+++ routes/new_mark_habit.js
@@ -5,23 +5,29 @@ var dbms = require("./dbms.js");
 
 router.post('/', function(req, res, next) {
     day = req.body.day;
-    console.log(day)
+    console.log("Day" + day);
     var today = new Date(day);
-    console.log(today)
+    console.log(today);
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1);
     var yyyy = today.getFullYear();
-    
-    today = mm + '-' + dd + '-' + yyyy;
-    console.log(today)
+    today = yyyy + '-' + mm + '-' + dd;
+
+
 
     var username = req.body.username;
     var habit = req.body.habit_name
-    
-    
+
+    console.log("Today");
+    console.log(today)
+    console.log("habit");
+    console.log(habit);
+    console.log("username");
+    console.log(username);
+
 dbms.dbquery("INSERT INTO  habit (username,adate,habit) VALUES ('"+username+"','"+today+"','"+habit+"')",
     function(err, data){ }
-    );        
+    );
 
 });
 module.exports = router;
