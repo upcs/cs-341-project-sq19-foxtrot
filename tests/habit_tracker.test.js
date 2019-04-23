c4938c0eaae5cd5e5a5cbb5638edb3565f2e8c31
Joanna Hoang <hoangj21@up.edu>
2019-04-23 16:13:47 -0700

    Altered habit tests

--- tests/habit_tracker.test.js
+++ tests/habit_tracker.test.js
@@ -43,14 +43,17 @@ describe('Testing login function that fail', () => {
 
 
     });
-/*
+
 test('row is added', () => {
-    $('#add_button').click();
     tableId= 'myTable'
-    var rows = document.getElementById(tableId).getElementsByTagName("tr").length;
+    var rowInitial = document.getElementById(tableId).getElementsByTagName("tr").length;
+    $('#add_button').click();
+    var rowAfter = document.getElementById(tableId).getElementsByTagName("tr").length;
     expect( habit.add_row()).toBeCalled();
-    //expect($(rows).toEqual(4));
+    rowAfterExpected = rowInitial + 1
+    expect($(rows).toEqual(rowAfterExpected));
 });
+/*
 test('row is removed', () => {
     $('#remove_button').click();
     tableId= 'myTable'
@@ -58,11 +61,11 @@ test('row is removed', () => {
 
     expect( habit.removeRow()).toBeCalled();
     //expect($(rows).toEqual(3));
-});
+});*/
 test('cell is marked', () => {
     $('#mark_button').click();
     cell= 'mark_button'
     var test = document.getElementById(cell).textContent;
     expect(habit.mark_cell()).toBeCalled();
     expect($(test).toEqual("Completed"));
-});*/
+});
