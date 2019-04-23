fb42b3bbb9988e68bf456ae993719f741026904e
ashika-m <mulagada21@up.edu>
2019-04-23 15:39:37 -0700

    Update habit_tracker.test.js

--- tests/habit_tracker.test.js
+++ tests/habit_tracker.test.js
@@ -16,22 +16,22 @@ describe('Testing login function that fail', () => {
 		habit.setCookie("tracker", "2", .001);
 		expect(habit.getCookie("tracker")).toBe("2");
     })
-    
+
       test('Get week number works with valid date', () => {
         expect(habit.getWeekNumber('2019-03-25T07:00:00.000Z')).toBeCalled;
         expect(habit.getWeekNumber('2019-03-25T07:00:00.000Z')).toBe(13);
       });
-    
+
       test('Get week number works with valid date early in the year', () => {
         expect(habit.getWeekNumber('2019-01-05T07:00:00.000Z')).toBeCalled;
         expect(habit.getWeekNumber('2019-01-05T07:00:00.000Z')).toBe(2);
       });
-    
+
       test('Get week number works with valid date late in the year', () => {
         expect(habit.getWeekNumber('2019-12-31T07:00:00.000Z')).toBeCalled;
         expect(habit.getWeekNumber('2019-12-31T07:00:00.000Z')).toBe(53);
       });
-    
+
       test('Get sunday works with valid date', () => {
         expect(habit.getSundayFromWeekNum(2, 2019)).toBeCalled;
       });
@@ -41,6 +41,13 @@ describe('Testing login function that fail', () => {
         expect(habit.removeTime('2019-12-31T07:00:00.000Z')).toBe('Tue, 31 Dec 2019');
       });
 
+
+      test('test that mark_prevCell changes the innerHTML', () => {
+        document.body.innerHTML =
+          ' ';
+        mark_prevCell(document.body);
+        expect(document.body.innerHTML.toBe('Completed'));
+      })
 
     });
 /*
